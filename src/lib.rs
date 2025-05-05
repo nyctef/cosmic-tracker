dce4f947bed1d3251b02bbc4e1641674394a4476
Nyctef <nyctef@nyctef.com>
2025-05-05 17:09:15 +0100

    remove old placeholder code

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,6 @@ use serde_wasm_bindgen;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[derive(Serialize)]
-struct TableRow {
-    cells: Vec<String>,
-}
-
 #[derive(Serialize)]
 struct WeatherPrediction {
     weather: String,
@@ -27,20 +18,6 @@ pub struct TimeData {
     utc_time: String,
     local_time: String,
     eorzean_time: String,
-}
-
-#[wasm_bindgen]
-pub fn get_table_data() -> JsValue {
-    let data = vec![
-        TableRow {
-            cells: vec!["Row 1, Col 1".to_string(), "Row 1, Col 2".to_string()],
-        },
-        TableRow {
-            cells: vec!["Row 2, Col 1".to_string(), "Row 2, Col 2".to_string()],
-        },
-    ];
-
-    serde_wasm_bindgen::to_value(&data).unwrap()
 }
 
 #[wasm_bindgen]
@@ -136,16 +113,5 @@ fn format_interval(d: Duration) -> String {
         format!("{}h{}m", hours, minutes)
     } else {
         format!("{}m", minutes)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
     }
 }
